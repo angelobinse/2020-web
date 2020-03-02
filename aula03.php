6c88cccd5ecf927a65dfcd54378f404637c5a44b
Aluno <huggiesgamer@gmail.com>
2020-03-02 09:27:33 -0300

    LANÇANDO SITTTEEEEE

---
+++ aula03.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+
+  <style media="screen">
+    .noticias h3{
+      color: red
+
+    }
+  </style>
+
+    <style media="screen">
+      body{
+        background-color: #f2f2f2
+
+      }
+    </style>
+  <style media="screen">
+    .noticias h3{
+      background-color: black
+
+    }
+  </style>
+
+  <style media="screen">
+    h2{
+      color: red
+
+    }
+  </style>
+
+  <style media="screen">
+    h2{
+      background-color: black
+
+    }
+  </style>
+
+  <style media="screen">
+    th{
+      background-color: #AAAAAA
+
+    }
+  </style>
+
+  <style media="screen">
+    tr{
+      background-color: #e6e6e6
+
+    }
+  </style>
+<h2>Portal de Noticias - IFPR</h2>
+<p>TO LANÇANDO MEU SIIIIIIIIIIIIIIIIIITTTTTTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</p>
+<div class="noticias">
+  <h3>Noticia 1</h3>
+
+
+  <p>Uma batida entre um carro e um ônibus do transporte público deixou cinco pessoas feridas,
+    neste sábado (21), na Cidade Industrial de Curitiba (CIC), de acordo com a Polícia Militar (PM).</p>
+
+  <p>O acidente aconteceu na Rua João Dembinski. Segundo a polícia, três pessoas que estavam
+    no ônibus tiveram ferimentos leves e foram levadas para o Hospital Evangélico.</p>
+
+  <p>O motorista do carro teve ferimentos considerados moderados, e o passageiro ficou gravemente ferido.
+      os dois foram levados para o Hospital do Trabalhador.</p>
+
+   <img src="https://s2.glbimg.com/Fvf7hA_QUykZHCE_Arg9wRbNgAU=/0x0:1280x720/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/G/y/Q4l4gzRIiE3VCb8htA4g/batida-carro-e-onibus.jpg" alt="">
+
+</div>
+
+<div class="noticias">
+  <h3>Noticia 2</h3>
+
+  <table style="width:100%">
+    <tr>
+      <th>Datas de Vencimento - IPVA</th>
+
+    </tr>
+    <tr >
+      <td>Final Placa</td>
+      <td>Parcela unica ou Primeira</td>
+      <td>Segunda Parcela</td>
+      <td>Terceira Parcela</td>
+    </tr>
+
+    <tr>
+      <td>1 e 2</td>
+      <td>17/02/2020</td>
+      <td>16/03/2020</td>
+      <td>13/04/2020</td>
+    </tr>
+
+    <tr>
+      <td>3 e 4</td>
+      <td>18/02/2020</td>
+      <td>17/03/2020</td>
+      <td>14/04/2020</td>
+    </tr>
+
+    <tr>
+      <td>5 e 6</td>
+      <td>19/02/2020</td>
+      <td>18/03/2020</td>
+      <td>15/04/2020</td>
+    </tr>
+
+    <tr>
+      <td>7 e 8</td>
+      <td>20/02/2020</td>
+      <td>19/03/2020</td>
+      <td>16/04/2020</td>
+    </tr>
+
+    <tr>
+      <td>9 e 0</td>
+      <td>21/02/2020</td>
+      <td>20/03/2020</td>
+      <td>17/04/2020</td>
+    </tr>
+
+
+  </table>
+
+  <p>O IPVA foi calculado de acordo com a tabela de preços médios da Fundação
+    Instituto de Pesquisas Econômicas (Fipe),
+    a Tabela Fipe. A receita prevista com o tributo é de R$ 1,16 bilhão.</p>
+
+<p>Os contribuintes do Distrito Federal foram surpreendidos nos últimos dias com a chegada
+     de um boleto para o pagamento à vista ou parcelado do IPVA) de 2020. Até então, o Governo do Distrito Federal (GDF) vinha divulgando que, neste ano,
+     a transação seria exclusivamente digital como forma de economizar recursos
+     públicos.</p>
+</div>
+<div class="noticias">
+  <h3>Noticia 3</h3>
+
+  <p>Após virar uma espécie de ponto turístico de Sobral, no interior do Ceará, o trator que o senador licenciado Cid Gomes (PDT-CE)
+    usou na última quarta-feira para quebrar o portão de um batalhão amotinado da Polícia Militar, voltou ontem para um canteiro de obras da Prefeitura próximo ao aeroporto da cidade.</p>
+
+  <p>A máquina passou o dia coberta por lona, já que os serviços de construção de jardins filtrantes, contratados pela administração municipal junto a três empresas, está suspenso durante o carnaval.</p>
+
+  <img src="https://img.olx.com.br/images/72/729011004218637.jpg" alt="">
+
+</div>
+
+</body>
+</html>
