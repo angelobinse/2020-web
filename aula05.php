a8e79fc8fd795f5fc7c95308d66dec765a21cb14
Aluno <huggiesgamer@gmail.com>
2020-03-16 09:55:39 -0300

    aula 05

---
+++ aula05.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>AULA 05 TO LANÇANDO MEU SIIIIIITTTTTTTTTEEEEEEEEEEEEEEEEEEEEEEEEEE</title>
+  <!-- Importando o css :) -->
+  <link rel="stylesheet" href="aula05.css">
+  <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+</head>
+<body>
+
+  <div class="site">
+
+    <div class="topo">
+      TOPO DO SITE
+
+      <!-- <h4>LANÇANDO MENU</h4> -->
+      <div class="menu">
+        <ul>
+          <li><a href="#">Pagina inicial</a></li>
+          <li><a href="#">Cadastro</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Ajuda</a></li>
+          <li><a href="#">Login</a></li>
+        </ul>
+
+      </div>
+    </div>
+    <div class="conteudo">
+        <!-- CORPO DO SITE -->
+
+        <!-- <h4>CRIAÇÂO DE BUTAO1</h4> -->
+        <a href="#"class="botao botao-azul">BOTAO AZUL</a>
+        <a href="#"class="botao botao-verde">BOTAO VERDE</a>
+        <a href="#"class="botao botao-vermelho">BOTAO VERMEI</a>
+
+
+
+    </div>
+    <div class="rodape">
+        Desenvolvido por Angelo Lindao
+
+
+    </div>
+
+  </div>
+
+</body>
+</html>
